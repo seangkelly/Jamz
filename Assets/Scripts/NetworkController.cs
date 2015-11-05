acc2719f65cb5edc6ae135edc79c17209ef9a827
Samanta <samantas@nyu.edu>
2015-11-04 20:27:36 -0500

    initial commit

---
+++ Assets/Scripts/NetworkController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class NetworkController : MonoBehaviour
+{
+	string _room = "Tutorial_Convrge";
+
+	//SPAWN SHITTTT
+	/*
+	private GameObject[] spawnPointList;
+	private int SPLength;
+
+	//AWAKE is for spawn setup
+	void Awake(){
+
+		//list of spawn points
+		spawnPointList = GameObject.FindGameObjectsWithTag ("SpawnPoint");
+		//size of array
+		SPLength = spawnPointList.Length;
+
+		Debug.Log ("this is kinda working");
+		Debug.Log (SPLength);
+
+
+	}
+	*/
+	void Start()
+	{
+		PhotonNetwork.ConnectUsingSettings("0.1");
+		Debug.Log ("Connected");
+
+
+	}
+	
+	void OnJoinedLobby()
+	{
+		Debug.Log("joined lobby");
+		
+		RoomOptions roomOptions = new RoomOptions() { };
+		PhotonNetwork.JoinOrCreateRoom(_room, roomOptions, TypedLobby.Default);
+	}
+	
+
+	void OnJoinedRoom()
+	{
+
+		//PhotonNetwork.Instantiate("CardboardMain", new Vector3(Random.Range(-5.0F, 5.0F), 0, Random.Range(-5.0F, 5.0F)), Quaternion.identity, 0);
+		PhotonNetwork.Instantiate("NetworkedPlayer", Vector3.zero, Quaternion.identity, 0);
+	}
+}
---
+++ Assets/Scripts/PlayParticles.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayParticles : Photon.MonoBehaviour {
+
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetKeyDown (KeyCode.Space))
+			//GetComponent<ParticleSystem>().Emit(300);
+		photonView.RPC("DoExploder",PhotonTargets.All, new object[]{15});
+
+
+		else if(Input.GetKeyDown (KeyCode.Return))
+			//GetComponent<ParticleSystem>().Emit(1500);
+		photonView.RPC("DoExploder",PhotonTargets.All, new object[]{1500});
+			
+	}
+
+	[PunRPC]
+	public void DoExploder (int count)
+	{
+		GetComponent<ParticleSystem>().Emit(count);
+
+	}
+}
